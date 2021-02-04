ea2a64489dc3f77df06fb2a02b1c45383b577e4d
Iresh Madushanka <ireshmadushanka03@gmail.com>
2021-02-05 00:46:24 +0530

    deployement build

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className="text-center py-3">Copyright &copy; ProShop</Col>
+          <Col className="text-center py-3">Copyright &copy; SIM-SHOP</Col>
         </Row>
       </Container>
     </footer>
